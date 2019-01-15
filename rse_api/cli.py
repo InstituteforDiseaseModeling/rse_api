49d4572f9b080c793828162f1ab05e84ee51fe09
Clinton Collins <clinton.collins@gmail.com>
2019-01-15 09:31:40 -0800

    Ensure we can set the host

--- rse_api/cli.py
+++ rse_api/cli.py
@@ -6,8 +6,9 @@ def add_cli(app):
     manage_cli = AppGroup('manage', help="Commands related to running application")
 
     @manage_cli.command('run', help='Runs the application')
+    @click.option('--host', default='127.0.0.1', help='What Host should the app run on')
     @click.option('--port', default=5000, help='What port should the server run on?')
-    def run(port):
-        app.run(port=port)
+    def run(host, port):
+        app.run(host=host, port=port)
 
     app.cli.add_command(manage_cli)
