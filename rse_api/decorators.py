3b27e425118cb0946e7b76c5e0a340082770cf5b
Clinton Collins <clinton.collins@gmail.com>
2019-01-31 09:50:42 -0800

    * move singleton function defintion order * Add swagger to decorators * Begin converting to google document style comments

--- rse_api/decorators.py
+++ rse_api/decorators.py
@@ -32,6 +32,22 @@ def json_only(func: Callable) -> Callable:
     return wrapper
 
 
+def singleton_function(func: Callable) -> Callable:
+    """
+    Allows a function to run once then cache its results for later calls
+    :param func: Function to be cached
+    :return: Wrapper function
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not hasattr(func, 'has_ran'):
+            setattr(func, 'cache_value', func(*args, **kwargs))
+            setattr(func, 'has_ran', True)
+        return getattr(func, 'cache_value')
+    return wrapper
+
+
 def register_crud(endpoint, url=None) -> Callable:
     if url is None:
         url = endpoint
@@ -59,6 +75,8 @@ def register_resource(urls):
     elif type(urls) is str:
         urls = [urls]
 
+    from rse_api.swagger.swagger_spec import get_swagger
+
     def decorator_register_resource(func: Callable) -> Callable:
         if not issubclass(func, Resource):
             raise RSEApiException(f"You can only register Resource derived classes using this decorator."
@@ -69,27 +87,134 @@ def register_resource(urls):
             return func(*args, **kwargs)
         resource_args = tuple([func] + urls)
         api.add_resource(*resource_args)
+
+        # process swagger after so urls have been registered
+        swagger = get_swagger()
+        swagger.process_class(func.__name__, urls)
         return wrapper
     return decorator_register_resource
 
 
-def schema_in(schema: Schema, many: bool=False) -> Callable:
+def schema_in(schema: Schema, many: bool=False, instance_loader_func: Callable=None, partial: bool = False) -> Callable:
     """
     Decorator that converts the flask json input data into a parsed data from a supplied schema object
 
-    :param schema: Marshmallow schema to parse flask request with. It is recommended to use strict mode schemas
-    :param many: Is the input expecting many objects?
-    :return: Wrapped function
-    """
 
+    Args:
+        schema: An instance of a Marshmallow Schema object that will be used to parse the input data's body
+        many: Controls whether input is expected to be an array of items or a single item of type Schena. Defaults to
+        *False*.
+        instance_loader_func: Function that takes the args for the endpoint and uses those to load an instance.
+    Usually this is used for PUTs to load and instance by id. See the PUT Example below
+        partial: To be used when a function is provided . If so, we support partial loads on puts. This means that the
+        user can supply single field updates since the db instance will be loaded.
+    Examples:
+        .. code-block:: python
+
+            from flask_restful import Resource
+            from marshmallow import fields, Schema
+            from rse_api.decorators import schema_in
+            from marshmallow_sqlalchemy import ModelSchema
+            from rse_db.utils import get_db, get_declarative_base
+            from rse_db.data_patterns import IdMixin, CreatedAndUpdatedAtMixin
+
+            Base = get_declarative_base()
+
+            # Our DB Model. It includes an autoincrementing id and Created/Updated timestamp fields
+            # We also add some basic Find, and Write functionality through RSEBasicReadWriteModel
+            class UserModel(IdMixin, CreatedAndUpdatedAtMixin, RSEBasicReadWriteModel, Base):
+                __tablename__ = 'users'
+                username = Column(Text, nullable=False)
+
+            # Vanilla marshmallow schema
+            # that will allow ensure input data matches schema
+            class UserSchema(Schema):
+                username = fields.String(required=True)
+
+            # Smart schema that will load automatically convert schema parsed content
+            # into a specified db Model
+            class SmartUserSchema(ModelSchema):
+                username = fields.String(required=True)
+
+                class Meta:
+                    model = TaskTag
+                    # We use get_db here vs db to reduce the chance of import loops
+                    # see documentation on get_db
+                    sqla_session = get_db().session
+
+            default_in_schema = UserSchema(exclude=[id])
+
+            @register_resource(['/users', '/users/<int:id>'])
+            class UserController(Resource):
+
+                # The below will dump a a parsed schema object
+                @schema_in(default_in_schema)
+                def post(self, data):
+                    # we should save our data and then return
+                    return data.data
+
+                # The below we dump the input data minus any id field passed through the body
+                @schema_in(default_in_schema)
+                def put(self, id, data):
+                    # we should load exiting data using id
+                    # then merge in the new data from our input
+                    # then save it
+                    return data.data
+
+
+            sm_in_schema = SmartUserSchema(exclude=[id])
+            sm_out_schema = SmartUserSchema()
+
+            @register_resource(['/sm', '/sm/<int:id>'])
+            class SmartUserController(Resource):
+
+                @schema_out(sm_out_schema)
+                def get(self, id=None):
+                    if id is None:
+                        return UserModel.query.all()
+                    else:
+                        return UserModel.find_one(id)
+
+                # This will load our data as an instance of UserModel ready to be committed
+                @schema_in_out(sm_in_schema, sm_out_schema)
+                def post(self, data):
+                    # We get an instance of UserModel which has a commit function thanks to
+                    # RSEBasicReadWriteModel
+                    return data.commit()
+
+                # This will be load the data based on the input from the URL being based
+                # to UserModel.find_one, if that succeeds we should get a UserModel back
+                # with the updates ready to be saved
+                @schema_in_out(sm_in_schema, sm_out_schema, instance_loader_func=UserModel.find_one, partial=True)
+                def put(self, data):
+                    return data.commit()
+
+                def delete(self, id):
+                    UserModel.delete_by_pk(id)
+                    return '', 204
+
+
+    Returns:
+        Wrapped function
+    """
     def decorate_schema_in(func: Callable):
         from flask import request
+
+        from rse_api.swagger.swagger_spec import get_swagger
+
+        swagger = get_swagger()
+        swagger_function = dict(function=func, partial=partial, many=many, in_schema=schema,
+                                has_instance_loader_func=callable(instance_loader_func))
+        swagger.add_schema_function(swagger_function)
 
         @wraps(func)
         @json_only
         def wrapper_schema_in(*args, **kwargs):
             body = request.json
-            result = schema.load(body, many=many)
+            if callable(instance_loader_func):
+                result = schema.load(body, many=many, instance=instance_loader_func(*args, **kwargs), partial=partial)
+            else:
+                result = schema.load(body, many=many)
             if hasattr(result, 'data'):
                 args = tuple(list(args) + [result.data]) if args is not None else (result.data,)
             else:
@@ -99,29 +224,39 @@ def schema_in(schema: Schema, many: bool=False) -> Callable:
     return decorate_schema_in
 
 
-def schema_out(schema: Schema, many=False) -> Callable:
+def schema_out(schema: Schema, detect_many=True, many=False) -> Callable:
     """
     Decorator that attempts to convert the output of the wrapped function with a Flask JSON Response using the
     supplied Marshmallow schema
 
     :param schema: Marshmallow schema to convert output of function to
-    :param many: Is the output expecting many objects?
+    :param detect_many: Detect if output should be many(lists)
+    :param many: Only needed if detect many is False. Mainly set the except many output
     :return: Wrapped function
     """
     def decorate_schema_out(func: Callable):
+        from rse_api.swagger.swagger_spec import get_swagger
+
+        swagger = get_swagger()
+        swagger_function = dict(function=func, detect_many=detect_many, many=many, out_schema=schema)
+        swagger.add_schema_function(swagger_function)
+
         @wraps(func)
         def wrapper_schema_out(*args, **kwargs):
             result = func(*args, **kwargs)
-            result = schema.dump(result, many=many)
+            imany = (detect_many and type(result) is list) or many
+            result = schema.dump(result, many=imany)
             if hasattr(result, 'data'):
-                return jsonify(schema.dump(result, many=many).data)
+                return jsonify(result.data)
             else:
                 return jsonify(result)
         return wrapper_schema_out
     return decorate_schema_out
 
 
-def schema_in_out(schemaIn: Schema, schemaOut: Schema, schema_in_many=False, schema_out_many=False) -> Callable:
+def schema_in_out(schemaIn: Schema, schemaOut: Schema, schema_in_many=False, schema_out_many=False,
+                  schema_out_detect_many: bool=True, schema_in_loader_func: Callable = None,
+                  schema_in_partial: bool=False) -> Callable:
     """
     Decorator for methods that take a schema result and return a object that will be serialized to a specific schema
 
@@ -135,31 +270,25 @@ def schema_in_out(schemaIn: Schema, schemaOut: Schema, schema_in_many=False, sch
     :return: Wrapped function
     :rtype: Callable
     """
+
     def decorate_schema_in_out(func: Callable):
+        from rse_api.swagger.swagger_spec import get_swagger
+
+        swagger = get_swagger()
+        swagger_function = dict(function=func, detect_many=schema_out_detect_many, in_many=schema_in_many,
+                                many=schema_out_many, partial=schema_in_partial,
+                                instance_loader_func=callable(schema_in_loader_func), in_schema=schemaIn,
+                                out_schema=schemaOut)
+        swagger.add_schema_function(swagger_function)
+
         @wraps(func)
-        @schema_in(schemaIn, schema_in_many)
-        @schema_out(schemaOut, schema_out_many)
+        @schema_in(schemaIn, schema_in_many, instance_loader_func=schema_in_loader_func, partial=schema_in_partial)
+        @schema_out(schemaOut, many=schema_out_many, detect_many=schema_out_detect_many)
         def wrapper(*args, **kwargs):
             return func(*args, **kwargs)
 
         return wrapper
     return decorate_schema_in_out
-
-
-def singleton_function(func: Callable) -> Callable:
-    """
-    Allows a function to run once then cache its results for later calls
-    :param func: Function to be cached
-    :return: Wrapper function
-    """
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if not hasattr(func, 'has_ran'):
-            setattr(func, 'cache_value', func(*args, **kwargs))
-            setattr(func, 'has_ran', True)
-        return getattr(func, 'cache_value')
-    return wrapper
 
 
 def timeit_logged(func: Callable) -> Callable:
---
+++ rse_api/swagger/swagger_spec.py
@@ -0,0 +1,370 @@
+import json
+import os
+import re
+from collections import defaultdict
+from enum import Enum
+from typing import Dict, Any, TextIO, Union, List
+
+import yaml
+from flask import Flask
+from marshmallow import Schema, fields
+from rse_api.decorators import singleton_function
+
+
+def find_rule_by_url(app: Flask, url):
+    for rule in app.url_map.iter_rules():
+        if rule.endpoint == url:
+            return rule.endpoint, rule.method
+    return None
+
+
+def find_view_func_by_url(app: Flask, url):
+    rule = find_rule_by_url(app, url)
+    if rule:
+        view = app.view_functions.get(rule.endpoint)
+        view.get(rule.method)
+    return None
+
+
+REFERENCE_PATH = "#/components/schemas/"
+
+
+class SwaggerSpecFormats(Enum):
+    YAML = 'yaml'
+    JSON = 'json'
+
+
+class SwaggerSpec:
+    def __init__(self):
+        path = os.path.dirname(os.path.abspath(__file__))
+        self.data = json.load(open(os.path.join(path, 'base_schema.json'), 'r'))
+        self.definitions = {}
+        self.paths = defaultdict(dict)
+        self.class_map = {}
+        self.url_expr = re.compile(r'(\<.*?\>)', re.DOTALL)
+        self.url_replace = re.compile(r'\<(.*?):(.*?)\>', re.DOTALL)
+        # This should hold a dict of dicts
+        self.function_map = defaultdict(dict)
+
+    def set_version(self, version):
+        self.data['version'] = version
+
+    def set_title(self, title):
+        self.data['title'] = title
+
+    def set_description(self, description):
+        self.data['description'] = description
+
+    def set_terms_of_service(self, tos):
+        self.data['termsOfService'] = tos
+
+    def set_host(self, servers: List[str]):
+        self.data['servers'] = servers
+
+    def set_base_path(self, path):
+        self.data['basePath'] = path
+
+    def set_contact_info(self, name, email, url=None):
+        self.data['contact'] = dict(
+            name=name,
+            email=email
+        )
+        if url:
+            self.data['url'] = url
+
+    def add_definition(self, obj_class: Schema):
+        self.definitions[obj_class.__name__] = obj_class
+
+    def add_schema_function(self, swagger_data: Dict[str, Any]):
+        # parse class name from the name
+        target_func = swagger_data['function']
+        if 'decorate' not in target_func.__qualname__:
+            cls_name, method = target_func.__qualname__.split('.')
+
+            data = dict(
+                class_name=cls_name,
+                method=method,
+                arguments=target_func.__code__.co_varnames,
+                defaults=target_func.__defaults__
+            )
+            data.update({k: v for k, v in swagger_data.items() if k not in ['function']})
+
+            self.function_map[cls_name][method] = data
+
+    def add_class_to_map(self, cls, url):
+        self.class_map[cls] = url
+
+    def parameters_from_url(self, url, obj_string):
+        url_parameters = []
+
+        for parameter in re.findall(self.url_expr, url):
+            parameter = re.sub(r"[\<\>]", '', parameter)
+            url_parameter = {
+                "in": "path",
+                "description": f'ID Of {obj_string}',
+                "required": True,
+                "type": "string"
+            }
+            if ":" in parameter:
+                parameter_type, parameter = parameter.split(":")
+                url_parameter['type'] = parameter_type
+                if url_parameter['type'] == "int":
+                    url_parameter['type'] = 'integer'
+                # Do this here so that we touch any without types
+                url_parameter['name'] = parameter
+                url_parameters.append(url_parameter)
+
+        return url_parameters
+
+    def schema_to_definition(self, definition_name, schema):
+        swagger_definition = {'type': 'object', 'properties': {}, 'required': []}
+
+        for field_name, field in schema.fields.items():
+            if field_name not in schema.exclude:
+                pr_field = None
+                # Update the required fields
+                if field.required:
+                    swagger_definition['required'].append(field.name)
+
+                field_type = type(field)
+                if field_type in [fields.String, fields.Str, fields.Email, fields.UUID, fields.DateTime, fields.Date,
+                                  fields.LocalDateTime, fields.Url, fields.URL, fields.Raw]:
+
+                    pr_field = {
+                        "type": "string"
+                    }
+                    if field_type in [fields.DateTime, fields.Date, fields.LocalDateTime]:
+                        pr_field['format'] = 'date' if field_type is fields.Date else 'date-time'
+                    elif field_type in [fields.URL, fields.Url]:
+                        pr_field['format'] = 'uri'
+                    elif field_type is fields.UUID:
+                        pr_field['format'] = 'uuid'
+                    # how do we define byte or binary fields?
+
+                elif field_type in [fields.Integer, fields.Int]:
+                    pr_field = {
+                        "type": "integer"
+                    }
+
+                elif field_type in [fields.Float, fields.Decimal, fields.Number, fields.FormattedString]:
+                    pr_field = {
+                        "type": "number"
+                    }
+                elif field_type in [fields.Boolean, fields.Bool]:
+                    pr_field = {
+                        "type": "boolean"
+                    }
+                elif field_type in [fields.Nested, fields.List, fields.Dict]:
+                    field_schema = None
+
+                    if field_type is fields.List:
+                        if type(field.container) is fields.Nested:
+                            # lookup object name
+                            field_schema = field.container.nested.__class__.__name__.replace('Schema', '')
+                            if field_schema not in self.definitions:
+                                self.definitions[field_schema] = field.container.nested, self.schema_to_definition(
+                                    field_schema, field.container.nested)
+                    else:
+                        if field.nested == "self":
+                            field_schema = definition_name
+                    pr_field = {
+                        "type": "object" if field_type is fields.Nested else "array"
+                    }
+                    if field_schema:
+                        pr_field["schema"] = {
+                            "$ref": f"{REFERENCE_PATH}{field_schema}"
+                        }
+                else:
+                    pr_field = {
+                        "type": field_type.__name__
+                    }
+                    pass
+
+                if pr_field:
+                    swagger_definition['properties'][field.name] = pr_field
+
+        return swagger_definition
+
+    def add_path_method(self, url, method, operation_id=None, parameters=None, in_schema=None, out_schema=None,
+                        description=None, many=True, errors=None, autogen_url_parameters=True):
+        if method not in self.paths[url]:
+            if in_schema:
+                obj_string = in_schema.__class__.__name__.replace('Schema', '')
+            elif out_schema:
+                obj_string = out_schema.__class__.__name__.replace('Schema', '')
+            else:
+                obj_string = None
+            if description is None:
+                if obj_string is None:
+                    raise NotImplementedError("Cannot auto-generate description without an input schema or an output "
+                                              "schema")
+
+                if method == "get" and out_schema and many:
+                    description = f"Returns all {obj_string}s from the system that the user has access to."
+                elif method == "get" and not many:
+                    description = f"Returns a {obj_string} based on an id."
+                elif method == "post" and in_schema:
+                    description = f"Creates a  {obj_string}"
+                elif method == "put" and in_schema and not many:
+                    description = f"Update a {obj_string} based on an id and user supplied input."
+                elif method == "delete" and not many:
+                    description = f"Delete a {obj_string} based on id"
+
+            if operation_id is None:
+                if method == "get" and out_schema and many:
+                    operation_id = f"findAll{obj_string}s"
+                elif method == "post":
+                    operation_id = f"find{obj_string}ById"
+                elif method == "put" and in_schema and not many:
+                    operation_id = f"update{obj_string} based on an id and user supplied input."
+                elif method == "delete" and not many:
+                    operation_id = f"remove{obj_string} based on id"
+
+            if parameters is None:
+                # try to parse parameters from url
+                parameters = []
+
+            if len(parameters) is 0 or autogen_url_parameters:
+                parameters += self.parameters_from_url(url, obj_string)
+
+            responses = {}
+            if out_schema:
+                swagger_schema = {
+                    "$ref": f"{REFERENCE_PATH}{obj_string}"
+                }
+
+                if many:
+                    responses[200] = dict(description=f"List of {obj_string}s response", content=dict(
+                        {
+                            'application/json': dict(schema={
+                                "type": "array",
+                                "items": swagger_schema
+                            })
+                        }
+                    )
+                                          )
+                    if obj_string not in self.definitions:
+                        self.definitions[obj_string] = out_schema, self.schema_to_definition(obj_string, out_schema)
+                else:
+                    responses[200] = dict(description=f"{obj_string} response",
+                                          content=dict(
+                                              {
+                                                  'application/json': dict(
+                                                      schema=swagger_schema
+                                                  )
+                                              }
+                                          ))
+
+            request_body = None
+            if in_schema:
+
+                action = 'add' if method == 'post' else 'update'
+                obj_prefix = 'new' if method == 'post' else 'partial'
+
+                swagger_obj_name = obj_string
+                if obj_string in self.definitions:
+                    if len(in_schema.exclude) != len(self.definitions[obj_string][0].exclude):
+                        swagger_obj_name = f'{obj_prefix}{obj_string}'
+
+                swagger_schema = {
+                    "$ref": f"{REFERENCE_PATH}{swagger_obj_name}"
+                }
+
+                request_body = {
+                    "in": "body",
+                    "name": f"{obj_string.lower()}",
+                    "description": f'{obj_string} to {action}',
+                    "required": True,
+                    "type": "string",
+                    "schema": swagger_schema
+                }
+
+                if swagger_obj_name not in self.definitions:
+                    self.definitions[swagger_obj_name] = in_schema, self.schema_to_definition(swagger_obj_name,
+                                                                                              in_schema)
+
+            # check url to make sure we shouldn't replace part of it
+            url = re.sub(self.url_replace, r'{\2}', url)
+
+            path_method = dict(
+                description=description,
+                operationId=operation_id,
+                parameters=parameters,
+                responses=responses,
+                default=dict(description="unexpected error",
+                             content=dict(
+                                 {
+                                     'application/json': dict(
+                                         schema={"$ref": f"{REFERENCE_PATH}Error"}
+                                     )
+                                 }
+                             )
+                             )
+            )
+            if request_body:
+                path_method['requestBody'] = request_body
+
+            self.paths[url][method] = path_method
+
+    def process_class(self, class_name, urls, exclude_put_without_id=True):
+        if class_name in self.function_map:
+            from rse_api import get_application
+            app = get_application()
+
+            for rule in app.url_map.iter_rules():
+                if rule.rule in urls:
+                    # now check if this url has entry in our function map
+                    for method in [r.lower() for r in rule.methods]:
+                        if method in self.function_map[class_name]:
+                            if method == "put" and len(rule.arguments) == 0:
+                                continue
+                            api_details = self.function_map[class_name][method]
+                            if 'out_schema' in api_details and 'in_schema' in api_details:
+                                many = (api_details['detect_many'] and len(rule.arguments) == 0) or api_details['many']
+                                self.add_path_method(rule.rule, method, in_schema=api_details['in_schema'],
+                                                     out_schema=api_details['out_schema'], many=many)
+                            elif 'out_schema' in api_details:
+                                many = (api_details['detect_many'] and len(rule.arguments) == 0) or api_details['many']
+                                self.add_path_method(rule.rule, method, out_schema=api_details['out_schema'],
+                                                     many=many)
+                            elif 'in_schema' in api_details:
+                                self.add_path_method(rule.rule, method, in_schema=api_details['in_schema'])
+
+    def generate_swagger_spec(self, file: Union[str, TextIO] = None,
+                              out_format: SwaggerSpecFormats = SwaggerSpecFormats.JSON):
+        spec = {
+        }
+
+        spec.update(self.data)
+        spec.update(dict(paths=self.paths))
+        spec.update(dict(
+            components=dict(
+                schemas={k: v[1] for k, v in self.definitions.items()}
+            )
+        ))
+        spec['components']['schemas']['Error'] = {
+            'type': 'object',
+            'properties': dict(code='integer', message='string'),
+            'required': ['code', 'message']
+        }
+
+        if file:
+            if type(file) is str:
+                file = open(file, 'w')
+            # either use yaml or json library dump
+            olib = yaml if out_format is SwaggerSpecFormats.YAML else json
+            olib.dump(spec, file)
+            file.close()
+        return spec
+
+
+@singleton_function
+def get_swagger() -> SwaggerSpec:
+    return SwaggerSpec()
+
+
+def register_swagger(app):
+    @app.before_first_request
+    def initialize_swagger():
+        swagger = get_swagger()
+        spec = swagger.generate_swagger_spec('test.yml')
